b7651993e6040e09e47d630b43ea2585d4837e4b
David Devoogdt <David.devoogdt@gmail.com>
2020-08-14 15:25:46 +0200

    implemented second block shemen and more general Hamiltonian

--- src/generateMPO.m
+++ src/generateMPO.m
@@ -4,7 +4,8 @@ classdef generateMPO
     
     properties
         d
-        H_tensor
+        H_1_tensor
+        H_2_tensor
     end
     
     properties (Access = private) 
@@ -12,11 +13,12 @@ classdef generateMPO
     end
     
     methods
-        function obj = generateMPO(d,H_tensor)
+        function obj = generateMPO(d,H_1_tensor,H_2_tensor)
             %GENERATEMPO Construct an instance of this class
             %   Detailed explanation goes here
             obj.d = d;
-            obj.H_tensor = H_tensor;
+            obj.H_1_tensor = H_1_tensor;
+            obj.H_2_tensor = H_2_tensor;
             obj.I_tensor = eye(d);
         end
         
@@ -142,10 +144,7 @@ classdef generateMPO
             %virtual levels
             %each seperate element is in ordering  upper legs.. lower legs
             
-            
-            
-           
-            
+
             MPO = mpo_cell_2_matrix(O,maxIndex,d);
             
             %seems to work
@@ -166,6 +165,120 @@ classdef generateMPO
          
             
         end
+        
+        
+        function MPO = type_02(obj, testing)
+            % Make a cell from O. It holds the tensor elements
+            % every entry is 4d nxdxdxm with n and m the bond dimension for the
+            % corresponding bond. dimension x1 at the end not shown by matlab
+            %this type generates no 1--|--1 and 2--|--2 blocks
+            d = obj.d;
+
+            maxIndex = 2;
+            O = cell(maxIndex,maxIndex);
+
+            O{1,1} = reshape(  obj.I_tensor, [1,d,d,1] ) ;
+
+            %step 1:
+            % 0--|--1--|--0 = exp(H_12) - (0--|--|--0 )  
+            N = 1;                  %number of free bonds
+            current_max_index = 0;  %used to contract the tensor 
+
+            RHS_Tensor_01 = H_exp(obj,N)- contract_O(N, O ,current_max_index,d);
+            RHS_Matrix_01 = reshape( permute( RHS_Tensor_01 , site_ordering_permute(N+1) ),...
+                                                        [d^2,d^2] ); %ready to svd
+
+            [U,S,V] = svd(RHS_Matrix_01);
+            sqrt_S = sqrt(S); %for symmetric split, not really necesary
+
+            O{0+1,1+1} = reshape( U*sqrt_S, [1,d,d,d^2]);
+            O{1+1,0+1} = reshape( sqrt_S* V', [d^2,d,d,1]);
+
+            if testing==1
+                err = tensor_norm( ncon( {O{0+1,1+1},O{1+1,0+1}}, {[-1,-2,-4,1],[1,-3,-5,-6]}, [1])-RHS_Tensor_01);
+                fprintf("err 01 = %d\n",err);
+            end
+
+            %step 2 :
+            % 0--|--1--|--2--|--0 = exp(H12+H23)- (0--|--|--|--0) 
+            N = 2;                  %number of free bonds
+            current_max_index = 1;  %used to contract the tensor 
+
+            RHS_Tensor_12 = H_exp(obj,N) - contract_O(N,O,current_max_index,d);
+            RHS_Matrix_12 = reshape(permute(RHS_Tensor_12, site_ordering_permute(N+1) ),...
+                                                          [d^2,d^2,d^2] ); 
+
+
+            %todo: this takes the inverse of the O_01 and O_10 matrices, could be done
+            % by solving a linear problem or starting from SVD
+
+            O_01_inv = reshape( O{1,2}, [d^2,d^2])^(-1);
+            
+            RHS_Tensor_120 = reshape( ncon( { O_01_inv, RHS_Matrix_12 }, { [-1,1], [1,-2,-3,-4]}  ),...
+                                       [d^4,d^2] );
+                
+            [U,S,V] = svd(RHS_Tensor_120);
+            %sqrt_S = sqrt(S); %for symmetric split, not really necesary
+
+            O{1+1,2+1} = reshape( U, [d^2,d,d,d^4]);
+            O{2+1,0+1} = reshape( S*V', [d^4,d,d,1]);
+                                   
+            if testing==1
+                err = tensor_norm( ncon( { O{1,2}, O{2,3}, O{3,1} }, {[-1,-2,-5,1],[1,-3,-6,2],[2,-4,-7,-8]}, [1,2])...
+                    -RHS_Tensor_12);
+                fprintf("err 0120 = %d\n",err);
+            end
+           
+            %step 2 :
+            % 0--|--1--|--2--|--1--|--0 = exp(H12+H23+h34)- (0--|--|--|--|--0) 
+            N = 3;                  %number of free bonds
+            current_max_index = 1;  %used to contract the tensor 
+
+            RHS_Tensor_121 = H_exp(obj,N) - contract_O(N,O,current_max_index,d);
+            RHS_Matrix_121 = reshape(permute(RHS_Tensor_121, site_ordering_permute(N+1) ),...
+                                                          [d^2,d^2,d^2,d^2] ); 
+
+
+            %todo: this takes the inverse of the O_01 and O_10 matrices, could be done
+            % by solving a linear problem or starting from SVD
+
+            
+            O_12_inv = reshape( O{2,3}, [d^4,d^4])^(-1);
+            O_10_inv = reshape( O{2,1}, [d^2,d^2])^(-1);
+            
+            temp = reshape(ncon( {O_01_inv, RHS_Matrix_121, O_10_inv }, { [-1,2] [2,-2,-3,3],[3,-4]}  ) ,...
+                                       [d^4,d,d,d^2 ] );
+                                   
+            O{3,2} =  ncon( {O_12_inv,temp}, {[-1,1],[1,-2,-3,-4]}  ) ;                    
+          
+                                   
+            if testing==1
+                err = tensor_norm( ncon( { O{1,2}, O{2,3}, O{3,2},O{2,1} }, {[-1,-2,-6,1],[1,-3,-7,2],[2,-4,-8,3] ,[3,-5,-9 ,-10 ]} )...
+                    -RHS_Tensor_121);
+                fprintf("err 01210 = %d\n",err);
+            end
+            
+            
+            
+            
+            MPO = mpo_cell_2_matrix(O,maxIndex,d);
+            
+            %seems to work
+            if testing==1
+                left =zeros(1,21);
+                left(1) = 1;
+                right =zeros(21,1);
+                right(1) = 1;
+                
+                err1 = tensor_norm( ncon( {left,MPO,right},{[-1,1],[1,-2,-3,2],[2,-4]})-contract_O(0,O,2,d));
+                err2 = tensor_norm( ncon( {left,MPO,MPO,right},{[-1,1],[1,-2,-4,2],[2,-3,-5,3],[3,-6]})-contract_O(1,O,2,d));
+                err3 = tensor_norm( ncon( {left,MPO,MPO,MPO,right},{[-1,1],[1,-2,-5,2],[2,-3,-6,3],[3,-4,-7,4],[4,-8]})-contract_O(2,O,2,d));
+                fprintf("matrixconversion  err0 %d err1 %d err3 %d \n",err1,err2,err3);
+            end
+            
+            
+        end
+        
 
     end
     
@@ -175,12 +288,41 @@ classdef generateMPO
         function H_exp = H_exp(obj,N)
             % return E(H_1_2+..+H_N-1_N) in normal ordering (dimension d^N+1, basis first
             % upper legs, then lower legs
-            % so this makes first the tensor T = H x I x I  
-            %                                  + I x H x I ...
+            % so this makes first the tensor T = H     x I x I  
+            %                                  + I x H     x I ...
+            %
+            %                                  + S x I x I x I       
+            %                                  + I x S x I x I ... 
+            % with S the single site operator and H the 2 site one
             % then reorders, and exponentiates
             d = obj.d;
             
             H = zeros( dimension_vector(d, 2*(N+1)) );
+            
+            %1 site operator
+            for i = 1:N+1
+                tensor_list = cell(1,N+1);
+                leg_list = cell(1,N+1);
+
+                for j = 1:i-1
+                    tensor_list{j} = eye(d);
+                    leg_list{j} = [-j ,-(N+1+j)];
+                end
+
+                tensor_list{i} = obj.H_1_tensor;
+                leg_list{i} = [-i, -(N+1+i)  ];
+
+                for j = i+1:N+1
+                    tensor_list{j} = eye(d);
+                    leg_list{j} = [-(j) ,-(N+1+j)];
+                end
+
+                H_i = ncon( tensor_list, leg_list, []);
+                H = H + H_i;
+            end
+            
+            
+            %2 site operator
             for i = 1:N
                 tensor_list = cell(1,N);
                 leg_list = cell(1,N);
@@ -190,7 +332,7 @@ classdef generateMPO
                     leg_list{j} = [-j ,-(N+1+j)];
                 end
 
-                tensor_list{i} = obj.H_tensor;
+                tensor_list{i} = obj.H_2_tensor;
                  leg_list{i} = [-i, -(i+1), -(N+1+i), -(N+i+2)  ];
 
                 for j = i+1:N
@@ -364,3 +506,4 @@ function T = mpo_cell_2_matrix(O,maxIndex,d)
 
 
 end
+
