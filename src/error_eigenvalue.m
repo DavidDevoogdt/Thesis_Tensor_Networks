87ae1211130508b7953b6a5dbc80903af80f6a9c
David Devoogdt <David.devoogdt@gmail.com>
2020-09-02 09:18:15 +0200

    changed to 1 norm

--- src/error_eigenvalue.m
+++ src/error_eigenvalue.m
@@ -48,42 +48,88 @@ function err = error_eigenvalue(mpo_1,type_01, mpo_2,type_02,M,d,opts)
     addParameter(p,'ref',0)
     parse(p,opts)
     
+    if p.Results.ref == 2
+        b=b*prefact_2;
+        a =a*prefact_1;
+        
+        %eig_b= eigs(b,1);
+        
+        
 
-    
-    
-    
-    
-    
-   tr_b = trace(b);
-   tr_a = trace(a);
+        %a = a*(  prefact_1/  prefact_2);
 
-   mag_2=tr_b*prefact_2;
-   mag_1=tr_a*prefact_1;
+        matrix= (a-b);
+    else 
+        
+       error("todo not implemented") ;
+    end
+        
+    %try
+    
+    [~,S1,~] = svd(a-b);
+    
+    sum1 = 0;
+    
+    for i=1:size(S1,1)
+        sum1= sum1+S1(i,i);   
+    end
+    
+    
+    [~,S2,~] = svd(b);
+    
+    sum2 = 0;
+    
+    for i=1:size(S2,1)
+        sum2= sum2+S2(i,i);   
+    end
+    
+    
+    if p.Results.ref ~= 0
+        err = sum1/sum2;
+        return
+    else
+        
+       error("not impl"); 
+    end
 
    
-   if mag_2 > mag_1 || p.Results.ref == 2
-       prefact = prefact_2*tr_b;
-       b = b/tr_b; %b normed to 1
-       a = a*(  prefact_1/ prefact);
-   else
-       prefact = prefact_2*tr_b;
-       a = a/tr_a; %b normed to 1
-       b = b*(  prefact_2/ prefact);
-   end
-                       
-    matrix= (a-b);
+%     catch
+%         warning('error = inf');
+%         err = Inf;
+%     end
     
-    try
-    	if p.Results.ref ~= 0
-            err = eigs(matrix,1);
-            return;
-        end
 
-        err = prefact*eigs(matrix,1);
-    catch
-        warning('error = inf');
-        err = Inf;
-    end
+
+%    tr_b = trace(b);
+%    tr_a = trace(a);
+% 
+%    mag_2=tr_b*prefact_2;
+%    mag_1=tr_a*prefact_1;
+% 
+%    
+%    if mag_2 > mag_1 || p.Results.ref == 2
+%        prefact = prefact_2*tr_b;
+%        b = b/tr_b; %b normed to 1
+%        a = a*(  prefact_1/ prefact);
+%    else
+%        prefact = prefact_2*tr_b;
+%        a = a/tr_a; %b normed to 1
+%        b = b*(  prefact_2/ prefact);
+%    end
+%                        
+%     matrix= (a-b);
+%     
+%     try
+%     	if p.Results.ref ~= 0
+%             err = eigs(matrix,1);
+%             return;
+%         end
+% 
+%         err = prefact*eigs(matrix,1);
+%     catch
+%         warning('error = inf');
+%         err = Inf;
+%     end
     
     
    
