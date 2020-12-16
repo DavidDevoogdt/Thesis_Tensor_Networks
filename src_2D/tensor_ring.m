c253f2ba886bb45ee6d7c9cdb89082e68b6a340b
David Devoogdt <david.devoogdt@gmail.com>
2020-12-16 20:24:48 +0100

    added fsolver and generalized tensor_ring solver

--- src_2D/tensor_ring.m
+++ src_2D/tensor_ring.m
@@ -3,8 +3,32 @@ function [elem_list,err] = tensor_ring(elem_list,map,target_MPS, opts )
   if ~isfield(opts,'optim')
       opts.optim = 1:map.N;
   end
-     
+  
+  %mapping if same element appears multiple times
+  if ~isfield(opts,'get_elem_num')
+       opts.get_elem_num = 1:map.N;
+  end
+  
+  %'fsolve' or 'matrix_inv'
+  if ~isfield(opts,'solve_type')
+       error("provide solve type")
+  end
+  
+
+ 
+    function elems = get_elem_list()
+        elems = cell(1,map.N);
+        for ii=1:map.N
+            elems{ii} = elem_list{ opts.get_elem_num(ii) } ;
+        end
+    end
+  
+  optim_lookup = zeros(map.N,1);
+  optim_lookup( opts.optim ) = 1;
+  
+  
   num_optim = size( opts.optim,2 );
+  current_norms = zeros( map.N,1 );
 
     physical_dims = size(target_MPS);
     physical_dim = physical_dims(1);
@@ -31,7 +55,7 @@ function [elem_list,err] = tensor_ring(elem_list,map,target_MPS, opts )
 
     function [A,x0_shape]= get_A(num)
         
-        temp_list = elem_list;
+        temp_list = get_elem_list();
         
         x0_shape = size(temp_list{num},[1,2,3,4,5,6]); %properly padded with ones
         
@@ -47,8 +71,6 @@ function [elem_list,err] = tensor_ring(elem_list,map,target_MPS, opts )
         
         ii = 0;
        
-        
-        
         if ~isempty(l)
             ii = ii+1; 
             pair = map.h_bonds{l};
@@ -107,33 +129,168 @@ function [elem_list,err] = tensor_ring(elem_list,map,target_MPS, opts )
         A = reshape( permute(A, perm_vector)  , [],prod(x0_shape(3:end)));
     end
     
+    function F = get_residual(num,target,x)
+
+        temp_list = get_elem_list();
+
+        for ii = 1:map.N
+            if opts.get_elem_num(ii) == num
+                temp_list{ii} = x;
+            end
+        end
+
+        A  = ncon( temp_list,map.leg_list);
+        
+        F = reshape(  permute(A,site_ordering_permute(map.N)), size(target)) -target;
+
+    end
+
+
+
+    function  optimize_norms()
+        norms_red = current_norms (current_norms>0 );
+        
+        [largest, argmax] = max(norms_red);
+        [smallest, argmin] = min(norms_red);
+        
+        if largest/smallest >2 
+            index_arr = [opts.optim(argmax), opts.optim(argmin)];
+            value_arr = [largest,smallest];
+            
+            for iindex = 1:2
+                l_index = index_arr(iindex);
+                
+                l=map.h_bond_l_lookup{l_index};
+                r=map.h_bond_r_lookup{l_index};
+                u=map.v_bond_u_lookup{l_index};
+                d=map.v_bond_d_lookup{l_index};
+
+                neighbours = [l_index];
+                othernorms = [value_arr(iindex)];
+
+                 if ~isempty(l)
+                    pair = map.h_bonds{l};
+                    other = pair(1);
+                    if optim_lookup(other) == 1
+                         neighbours=[ neighbours,other];
+                         othernorms = [othernorms, current_norms(other)  ];
+                    end
+                end
+
+                if ~isempty(u)
+                    pair = map.v_bonds{u};
+                    other = pair(1);
+                    if optim_lookup(other) == 1
+                         neighbours=[ neighbours,other];
+                         othernorms = [othernorms, current_norms(other)  ];
+                    end
+                end
+
+                if ~isempty(r)
+                    pair = map.h_bonds{r};
+                    other = pair(2);
+                    if optim_lookup(other) == 1
+                         neighbours=[ neighbours,other];
+                         othernorms = [othernorms, current_norms(other)  ];
+                    end
+                end
+
+                if ~isempty(d)
+                    pair = map.v_bonds{d};
+                    other = pair(2);
+                    if optim_lookup(other) == 1
+                         neighbours=[ neighbours,other];
+                         othernorms = [othernorms, current_norms(other)  ];
+                    end
+                end
+
+                ns = size(othernorms,2);
+                avg_norm = prod(othernorms)^(1/ns);
+
+                for m=1:ns
+                   num =  neighbours(m);
+                   n =   current_norms(num);
+                   fact = avg_norm/n;
+                   current_norms(num) = avg_norm;
+                   elem_list{opts.get_elem_num(num)} = elem_list{opts.get_elem_num(num)}*fact;
+                end
+            end
+            
+            optimize_norms(); %call recursive
+        end
+    end
+
     err = Inf;
     for i=1:opts.maxit
  
         curr_ind = mod_index(i,num_optim);
         curr_elem = opts.optim(curr_ind);
+        
+        elem_index = opts.get_elem_num(curr_elem);
+        
+        
+        switch opts.solve_type{curr_elem}
+            case 'fsolve'
+                
+                %for fsolve only unique subprobs should be solved
+                
+                if  elem_index ==  curr_elem
 
-        if opts.print_level ==1
-           if curr_ind==1
-                fprintf("round %.4d err %.4e \n", (i-1)/num_optim ,err);
-           end
+                    options = optimoptions('fsolve','Display','iter');
+
+                    x = fsolve( @(x) get_residual(curr_elem,target_MPS,x),  elem_list{ elem_index }, options );
+                    normx = norm( reshape(x,[],1));
+
+                    F = get_residual(curr_elem,target_MPS,x);
+                    
+                    err = sum(reshape( F,[],1).^2).^(0.5);
+        
+                    current_norms(opts.get_elem_num(curr_elem)) = normx;
+                    elem_list{opts.get_elem_num(curr_elem) } = x;
+                    
+                end
+                    
+            case 'matrix_inv'
+        
+                B = rotate_rhs(target_MPS,curr_elem);
+                [A,x0_shape] = get_A(curr_elem); 
+
+                x=lsqminnorm(A,B);
+
+                F= A*x-B;
+                
+                normx = norm(x);
+
+                x = permute(reshape(x, [x0_shape(3),x0_shape(4),x0_shape(5),x0_shape(6),x0_shape(1),x0_shape(2)]),[5,6,1,2,3,4]); %put phys dims back in front
+
+                err = sum(reshape( F,[],1).^2).^(0.5);
+        
+                current_norms(opts.get_elem_num(curr_elem)) = normx;
+                elem_list{opts.get_elem_num(curr_elem) } = x;
+                
+            
+            otherwise 
+                error("unknown solve type")
         end
         
-        B = rotate_rhs(target_MPS,curr_elem);
-        [A,x0_shape] = get_A(curr_elem); 
+ 
         
-        x=lsqminnorm(A,B);
-        elem_list{curr_elem} = permute(reshape(x, [x0_shape(3),x0_shape(4),x0_shape(5),x0_shape(6),x0_shape(1),x0_shape(2)]),[5,6,1,2,3,4]); %put phys dims back in front
+        if curr_ind==num_optim
+            if opts.print_level ==1
+                
+                fprintf("round %.4d err %.4e \n", (i)/num_optim ,err);
+            end
+           optimize_norms();
+        end
         
-        err = sum(reshape( A*x-B,[],1).^2).^(0.5);
         
-        if err<opts.tol
+        if err <opts.tol && i>num_optim
             break
             
         end
-        
-       
+    
     end
+    optimize_norms();
 
 end
 
--- src_2D/tensor_ring_test.m
+++ src_2D/tensor_ring_test.m
@@ -1,18 +1,30 @@
-% mapopts.numbered=0;
-% mapopts.v_cyclic=1;
-% mapopts.h_cyclic=1;
+% %%%%%%%%%%%%%%%%5
+mapopts.numbered=0;
+mapopts.v_cyclic=0;
+mapopts.h_cyclic=1;
+map = PEPO.create_map([1,1],mapopts); %cyclic ring
+
+physdim=2;
+
+target_mps=  rand(physdim^2,physdim^2);
+
+opts.tol = 1e-13;
+opts.maxit = 1;
+opts.print_level = 1;
+opts.get_elem_num = [1;1];
+opts.solve_type = {'fsolve','fsolve'};
+
+
+elem_list = cell(1,1);
+
+bond_dim=4;
+
+elem_list{1} = rand(  physdim,physdim, bond_dim,1,bond_dim,1);
+[elems,err] = tensor_ring(elem_list,map,target_mps, opts);
+
+
+% %%%%%%%%%%%%%%%%%
 % 
-% map = PEPO.create_map([0,0,1,1;
-%                        1,1,1,1;
-%                        0,0,1,1;],mapopts); %cyclic ring
-% 
-
-% mapopts.numbered=0;
-% mapopts.v_cyclic=0;
-% mapopts.h_cyclic=1;
-%map = PEPO.create_map([1,1,1,1],mapopts); %cyclic ring
-
-
 %fit random 1 loop
 
 mapopts.numbered=1;
@@ -25,34 +37,34 @@ map = PEPO.create_map([1,2;
 physdim = 2;
 
 target_mps=  rand(physdim^2,physdim^2,physdim^2,physdim^2);
-bond_dim=6;
 
-opts.tol = 1e-13;
-opts.maxit = 5000;
-opts.print_level = 1;
+opts1.tol = 1e-13;
+opts1.maxit = 5000;
+opts1.print_level = 1;
+opts1.solve_type = {'matrix_inv','matrix_inv','matrix_inv','matrix_inv'};
 
 
 elem_list = cell(1,4);
 
-bond_dim_h=9;
-bond_dim_v=9;
+bond_dim_h=10;
+bond_dim_v=10;
 
 elem_list{1} = rand(  physdim,physdim, 1,1,bond_dim_h,bond_dim_v);
 elem_list{2} = rand(  physdim,physdim, bond_dim_h,1,1,bond_dim_v);
 elem_list{3} = rand(  physdim,physdim, 1,bond_dim_v,bond_dim_h,1);
 elem_list{4} = rand(  physdim,physdim, bond_dim_h,bond_dim_v,1,1);
 
-
-[elems,err] = tensor_ring(elem_list,map,target_mps, opts);
+[elems,err] = tensor_ring(elem_list,map,target_mps, opts1);
 
 %firt double lopp
 
 target_mps_2=  rand(physdim^2,physdim^2,physdim^2,physdim^2,physdim^2,physdim^2);
 
-opts2.tol = 1e-13;
+opts2.tol = 1e-12;
 opts2.maxit = 5000;
 opts2.print_level = 1;
 opts2.optim = [5,6];
+opts2.solve_type = {'','','','','matrix_inv','matrix_inv'};
 
 elem_list_2 = cell(1,6);
 
