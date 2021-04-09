1b37e7f988063c26cf1cc410d09f71d79a429a96
David Devoogdt <david.devoogdt@gmail.com>
2021-04-09 13:15:44 +0200

    subleading finite size scaling fit

--- src_2D/fit/dofit.m
+++ src_2D/fit/dofit.m
@@ -8,82 +8,176 @@ names = {
     'Ising2D_g=2.5000e+00_chi=23_02_April_2021_09:38';
     'Ising2D_g=2.5000e+00_chi=32_02_April_2021_09:38';
     'Ising2D_g=2.5000e+00_chi=45_02_April_2021_09:38';
-};
+    };
+Tc = 1.2736;
+% 
+% names = {
+%     'Ising2D_g=1.5000e+00_chi=8_02_April_2021_11:17';
+%     'Ising2D_g=1.5000e+00_chi=11_02_April_2021_11:17';
+%     'Ising2D_g=1.5000e+00_chi=16_02_April_2021_11:17';
+%     'Ising2D_g=1.5000e+00_chi=23_02_April_2021_11:17';
+%     'Ising2D_g=1.5000e+00_chi=32_02_April_2021_11:17';
+%     'Ising2D_g=1.5000e+00_chi=45_02_April_2021_11:17';
+%     'Ising2D_g=1.5000e+00_chi=64_02_April_2021_11:17';
+%     };
+% Tc = 1.98;
+
+% names ={
+%     'Ising2D_g=0.0000e+00_chi=8_02_April_2021_11:46';
+%     'Ising2D_g=0.0000e+00_chi=11_02_April_2021_11:46';
+%     'Ising2D_g=0.0000e+00_chi=16_02_April_2021_11:46';
+%     'Ising2D_g=0.0000e+00_chi=23_02_April_2021_11:46';
+%     'Ising2D_g=0.0000e+00_chi=32_02_April_2021_11:46';
+%     'Ising2D_g=0.0000e+00_chi=45_02_April_2021_11:46';
+%     }
+% Tc = 2 / log(1 + sqrt(2));
 
 
+names = {{
+    'Ising2D_g=0.0000e+00_chi=8_08_April_2021_09:52';
+    'Ising2D_g=0.0000e+00_chi=11_08_April_2021_10:11';
+    'Ising2D_g=0.0000e+00_chi=16_08_April_2021_10:43';
+    'Ising2D_g=0.0000e+00_chi=23_08_April_2021_11:50';
+    'Ising2D_g=0.0000e+00_chi=32_08_April_2021_14:41';
+    'Ising2D_g=0.0000e+00_chi=45_08_April_2021_18:59';
+    'Ising2D_g=0.0000e+00_chi=64_09_April_2021_05:59';
+    },{
+    'Ising2D_g=1.5000e+00_chi=8_08_April_2021_10:15';
+    'Ising2D_g=1.5000e+00_chi=11_08_April_2021_10:28';
+    'Ising2D_g=1.5000e+00_chi=16_08_April_2021_10:48';
+    'Ising2D_g=1.5000e+00_chi=23_08_April_2021_11:29';
+    'Ising2D_g=1.5000e+00_chi=32_08_April_2021_12:19';
+    'Ising2D_g=1.5000e+00_chi=45_08_April_2021_16:50';
+    'Ising2D_g=1.5000e+00_chi=64_09_April_2021_04:35';
+    },{
+    %'Ising2D_g=2.5000e+00_chi=8_08_April_2021_09:52';
+    %'Ising2D_g=2.5000e+00_chi=11_08_April_2021_10:02';
+    'Ising2D_g=2.5000e+00_chi=16_08_April_2021_10:17';
+    'Ising2D_g=2.5000e+00_chi=23_08_April_2021_10:52';
+    'Ising2D_g=2.5000e+00_chi=32_08_April_2021_12:39';
+    'Ising2D_g=2.5000e+00_chi=45_08_April_2021_17:46';
+    'Ising2D_g=2.5000e+00_chi=64_09_April_2021_05:52';
+    },{
+    'Ising2D_g=2.9000e+00_chi=8_08_April_2021_09:52';
+    'Ising2D_g=2.9000e+00_chi=11_08_April_2021_10:03';
+    'Ising2D_g=2.9000e+00_chi=16_08_April_2021_10:06';
+}};
 
+T_c_arr = [  2/log( 1+ sqrt(2) )   ,1.980,1.2737, 0.9  ];
+
+T_c_noise = rand*0.01;
+
+
+select = 1;
+Tc = T_c_arr(select);
+names = names{select};
 
 delta = [];
-M=[];
-S=[];
-T=[];
-xi=[];
-
+M = [];
+S = [];
+T = [];
+xi = [];
 
 for i = 1:numel(names)
     data = fetch_matfiles(names{i}, struct);
     data = filter_ising_results(data, struct);
-    
-    marek_arr =1/3*real( (data.eps_i(:, 6)- data.eps_i(:, 1))/3+...
-                         (data.eps_i(:, 5)- data.eps_i(:, 2))/2+...
-                         (data.eps_i(:, 4)- data.eps_i(:, 3)));
-  
-    delta = [delta;marek_arr];
-    M=[M; data.m ];
-    S=[S;data.S];
-    T=[T;data.T];
-    xi=[xi; real(1./data.inv_corr_length)];
+
+    marek_arr = real((data.eps_i(:, 6) - data.eps_i(:, 1)) / 3 + ...
+        (data.eps_i(:, 5) - data.eps_i(:, 2)) / 2 + ...
+        (data.eps_i(:, 4) - data.eps_i(:, 3)));
+
+    delta = [delta; marek_arr];
+    M = [M; data.m];
+    S = [S; data.S];
+    T = [T; data.T];
+    xi = [xi; real(1 ./ data.inv_corr_length)];
 end
 
+%% initialise
+%Tc = 2 / log(1 + sqrt(2)); nu = 1; beta = 1/8; c = 1/2;
+
+nu = 1; beta = 1/8; c = 1/2;
+
+%universal
+s_omega = 1;
+s_phi = 1;
+%nonuniversal
+s_c = 1e-4;
+s_d = 1e-4;
+
+%% set dof for scaling functions
+Nm = 3; Nxi = 3; NS = 2;
 
 
 %% set what will be fitted: Tc, exponents: nu,beta,c, orthogonal distance to scaling function
 FitTc = 1;
 Fitexp = 0;
-Orthdist = 0;
+Orthdist = 1;
+Subleading = 1; %https://arxiv.org/pdf/cond-mat/0505194.pdf
 
-%% initialise
-%Tc = 2 / log(1 + sqrt(2)); nu = 1; beta = 1/8; c = 1/2;
+FitS=1;
 
-Tc = 1.2736; nu = 1; beta = 1/8; c = 1/2;
-
-
-%% set dof for scaling functions
-Nm = 2; Nxi = 2; NS = 2;
-
-%% the rest
-param = 1e-3 * randn(1, 9 + 8 * Nm + 8 * Nxi + 8 * NS);
-param(1) = Tc; param(2) = nu; param(3) = beta; param(4) = c;
-
+%first round
 fitTc = 1;
 fitexp = 0;
 orthdist = 0;
+subleading = 0;
+
+
+extra_params = 6+6;
+
+%% the rest
+if FitS==1
+    param = 1e-3 * randn(1, 9 +12+ 8 * Nm + 8 * Nxi + 8 * NS);
+else
+    param = 1e-3 * randn(1, 9 +12+ 8 * Nm + 8 * Nxi);
+end
+
+
+param(1) = Tc+T_c_noise; param(2) = nu; param(3) = beta; param(4) = c;
+
+
+
+%param(5:6)= [s_omega,s_phi];
+param(5:16) = zeros(12,1);
+param(6:2:16)=1;
+
+
 
 % iterate until restarts don't cause improvements anymore
 Er = inf; Param = param; tt = tic; tic;
 while true
-    %try
-    [param, error] = fitt(T, delta, xi, M, S, fitTc, fitexp, orthdist, param, Nm, Nxi, NS, 200);
+    try
+    [param, error] = fitt(T, delta, xi, M, S, fitTc, fitexp,FitS, orthdist, subleading, param, Nm, Nxi, NS, 50);
+    catch
+        error = Er+1;
+    end
+    
     if error < Er
         display(['error: ', num2str(error), ' , critical point: ', num2str(param(1), 10), ' , TC: ', num2str(Tc, 10)])
         display(['nu: ', num2str(param(2), 10), ', NU: ', num2str(nu, 10), ', beta: ', num2str(param(3), 10), ', BETA: ', num2str(beta, 10)])
-        if abs(param(1) - Param(1)) / abs(param(1)) > 1e-8
-            toc; tic
+        if subleading == 1
+            s_omega = param(6);s_phi = param(8);
+            
+            fprintf("subleading:  omega %.2f  phi %.2f\n",s_omega,s_phi);
         end
+        
         Param = param; Er = error;
         disp(' ')
     end
-    %catch
-    %display('memory error')
-    %end
+
+
     if toc > 60 * 60 * 3
         break
     end
-    if rand(1) < 0.2
-        param = Param .* (1 + 1e-4 * randn(size(param))) + 1e-5 * randn(size(param));
-    else
-        param = param .* (1 + 1e-4 * randn(size(param))) + 1e-5 * randn(size(param));
-    end
+    %if rand(1) < 0.2
+        param = Param .* (1 + 1e-3 * randn(size(param))) + 1e-5 * randn(size(param));
+    %else
+    %    param = param .* (1 + 1e-4 * randn(size(param))) + 1e-5 * randn(size(param));
+    %end
+    
+    %param = param .* (1 + 1e-4 * randn(size(param))) + 1e-4 * randn(size(param));
+    
     if ~fitTc
         param(1) = Tc;
     end
@@ -94,6 +188,7 @@ while true
     fitTc = FitTc;
     fitexp = Fitexp;
     orthdist = Orthdist;
+    subleading = Subleading;
 
     if toc(tt) > 60 * 60 * 24 * 7
         break
--- src_2D/fit/fitt.m
+++ src_2D/fit/fitt.m
@@ -1,4 +1,4 @@
-function [param, resnorm] = fitt(T, delta, xi, m, S, fitTc, fitexp, orthdist, initials, Nm, Nxi, NS, maxit)
+function [param, resnorm] = fitt(T, delta, xi, m, S, fitTc, fitexp,FitS, orthdist, subleading, initials, Nm, Nxi, NS, maxit)
     T = T(:);
     xi = xi(:);
     m = m(:);
@@ -17,7 +17,7 @@ function [param, resnorm] = fitt(T, delta, xi, m, S, fitTc, fitexp, orthdist, in
     options = optimoptions('lsqnonlin', 'Display', 'iter');
     options.FunctionTolerance = 1e-12;
     options.OptimalityTolerance = 1e-12;
-    options.StepTolerance = 1e-12;
+    options.StepTolerance = 1e-9;
     options.MaxFunctionEvaluations = 1e8;
     options.MaxIterations = maxit;
     if fitTc
@@ -64,57 +64,116 @@ function [param, resnorm] = fitt(T, delta, xi, m, S, fitTc, fitexp, orthdist, in
         end
 
         %% x0 is the position of of the discontinuity of scaling functions
-        x0 = x(5);
+        x0 = x(5+12);
 
         %% make scaling functions and derivatives of scaling functions
-        sfunparam = x(6:9 + 8 * Nm);
-        [mf, dmf, fl1, fr1, gl1, gr1] = ScalingFunction(1, 1, x0, beta, sfunparam(1), sfunparam(2), sfunparam(3), sfunparam(4), Nm, sfunparam(5:(4 + 8 * Nm)), fl1, fr1, gl1, gr1);
+        sfunparam = x(12+6:12+9 + 8 * Nm);
+        [mf, dmf, fl1, fr1, gl1, gr1] = ScalingFunction(1, 1, x0, beta/nu, sfunparam(1), sfunparam(2), sfunparam(3), sfunparam(4), Nm, sfunparam(5:(4 + 8 * Nm)), fl1, fr1, gl1, gr1);
 
-        sfunparam = x(6 + 8 * Nm:9 + 8 * Nm + 8 * Nxi);
-        [xif, dxif, fl2, fr2, gl2, gr2] = ScalingFunction(-1, 1, x0, nu, sfunparam(1), sfunparam(2), sfunparam(3), sfunparam(4), Nm, sfunparam(5:(4 + 8 * Nm)), fl2, fr2, gl2, gr2);
+        sfunparam = x(12+6+ 8 * Nm:12+9+ 8 * Nm + 8 * Nxi);
+        [xif, dxif, fl2, fr2, gl2, gr2] = ScalingFunction(-1, 1, x0, 1/nu, sfunparam(1), sfunparam(2), sfunparam(3), sfunparam(4), Nxi, sfunparam(5:(4 + 8 * Nxi)), fl2, fr2, gl2, gr2);
 
-        sfunparam = x(6 + 8 * Nm + 8 * Nxi:9 + 8 * Nm + 8 * Nxi + 8 * NS);
-        [Sf, dSf, fl3, fr3, gl3, gr3] = ScalingFunction(-1, 1, x0, nu, sfunparam(1), sfunparam(2), sfunparam(3), sfunparam(4), Nm, sfunparam(5:(4 + 8 * Nm)), fl3, fr3, gl3, gr3);
-        dSf = @(x)dSf(x) ./ Sf(x);
-        Sf = @(x)log(Sf(x));
-
+        if FitS == 1   
+            sfunparam = x(6+12 + 8 * Nm + 8 * Nxi:9+12 + 8 * Nm + 8 * Nxi + 8 * NS);
+            [Sf, dSf, fl3, fr3, gl3, gr3] = ScalingFunction(-1, 1, x0, 1/nu, sfunparam(1), sfunparam(2), sfunparam(3), sfunparam(4), NS, sfunparam(5:(4 + 8 * NS)), fl3, fr3, gl3, gr3);
+            %dSf = @(x)dSf(x) ./ Sf(x);
+            %Sf = @(x)log(Sf(x));
+        end
         %% make dimensionless quantities
         t = T - Tc;
 
-        tt = t .* delta.^(-1 / nu); trange = max(abs(tt));
-        mt = (m .* delta.^(-beta / nu)); mrange = max(mt) - min(mt);
-        xit = (xi .* delta.^(1 / nu)); xirange = max(xit) - min(xit);
-        St = S + c / 6 * log(delta); Srange = max(St) - min(St);
+        if ~subleading
+            tt = t .* delta.^(-1 / nu); trange = max(abs(tt));
+            s_corr = 1+0*delta;
+            
+            tt1=tt;tt2=tt;tt3=tt;
+            trange1=trange;trange2=trange;trange3=trange;
+            s_corr1=s_corr;s_corr2=s_corr;s_corr3=s_corr;
 
-        tt = tt / trange; mt = mt / mrange; xit = xit / xirange; St = St / Srange;
+        else
+           
+            
+           
+            
+            pp = x(5:8); s_c_1=pp(1);omega=pp(2);s_d_1=pp(3);phi=pp(4);
+            s_corr1 = 1+s_c_1*delta.^(omega/nu)  ;
+            tt1 = t .* delta.^(-1 / nu) - s_d_1*delta.^(phi/nu) ; trange1 = max(abs(tt1));
+            
+            pp = x(9:12); s_c_1=pp(1);s_d_1=pp(3);
+            s_corr2 =  1+s_c_1*delta.^(omega/nu)  ;
+            tt2 = t .* delta.^(-1 / nu) - s_d_1*delta.^(phi/nu) ; trange2 = max(abs(tt2));
+            
+            if FitS == 1 
+                pp = x(13:16);  s_c_1=pp(1);s_d_1=pp(3);
+                s_corr3 = 1+s_c_1*delta.^(omega/nu)  ;
+                tt3 = t .* delta.^(-1 / nu) - s_d_1*delta.^(phi/nu) ; trange3 = max(abs(tt3));
+            else
+                tt3 = t .* delta.^(-1 / nu)  ; trange3 = max(abs(tt3));
+            end
+
+        end
+
+        mt = (m .* delta.^(-beta / nu))./s_corr1   ; mrange = max(mt) - min(mt);
+        xit = (xi .* delta.^(1 / nu))./s_corr2; xirange = max(xit) - min(xit);
+        
+        if FitS == 1 
+            St = exp( 6/c*S ).*delta ./s_corr3; Srange = max(St) - min(St);
+            St = St / Srange;
+        else
+            St = exp( 6/c*S ).*delta; Srange = max(St) - min(St);
+            St = St / Srange;
+        end
+       
+        
+        mt = mt / mrange; xit = xit / xirange; 
 
         if isempty(ttxi)
-            ttxi = tt; ttm = tt; ttS = tt;
+            ttxi = tt1; ttm = tt2;ttS = tt3;
+           
         end
 
         if orthdist
-            [ttm, resm] = DistanceToLine(mf, dmf, x0, ttm, tt, mt);
-            [ttxi, resxi] = DistanceToLine(xif, dxif, x0, ttxi, tt, xit);
-            [ttS, resS] = DistanceToLine(Sf, dSf, x0, ttS, tt, St);
+            [ttm, resm] = DistanceToLine(mf, dmf, x0, ttm, tt1, mt);
+            [ttxi, resxi] = DistanceToLine(xif, dxif, x0, ttxi, tt2, xit);
+            if FitS == 1 
+                [ttS, resS] = DistanceToLine(Sf, dSf, x0, ttS, tt3, St);
+            end
         else
-            resm = mt - mf(tt);
-            resxi = xit - xif(tt);
-            resS = St - Sf(tt);
+            resm = mt - mf(tt1);
+            resxi = xit - xif(tt2);
+            if FitS == 1 
+                resS = St - Sf(tt3);
+            end
         end
 
         %% distance from points to scaling function vector
-        res = [resm; resxi; resS];
+        if FitS == 1 
+            res = [resm; resxi; resS];
+        else
+            res = [resm; resxi];
+        end
 
         %% plot
         if mod(teller - 1, numel(x)) == 0
-            ttt = linspace(min(tt), max(tt), 200)';
-            tt = tt * trange; mt = mt * mrange; xit = real(xit * xirange); St = St * Srange;
+            ttt1 = linspace(min(tt1), max(tt1), 200)';
+            ttt2 = linspace(min(tt2), max(tt2), 200)';
+            
+            ttt3 = linspace(min(tt3), max(tt3), 200)';
+            
+            tt1 = tt1 * trange1;
+            tt2 = tt2 * trange2;
+            tt3 = tt3 * trange3;
+            
+            mt = mt * mrange; xit = xit * xirange; 
+            
+            St = St * Srange;
+            
             clf
             subplot(1, 3, 1)
             hold off
-            plot(tt, mt, '.b', 'MarkerSize', 4);
+            plot(tt1, mt, '.b', 'MarkerSize', 4);
             hold on
-            plot(ttt * trange, (mrange * mf(ttt)), '-g');
+            plot(ttt1 * trange1, (mrange * mf(ttt1)), '-g');
             hold off
             %title('correlation length scaling function');
             xlabel('(T-T_c)\delta^{-1/\nu}');
@@ -123,9 +182,9 @@ function [param, resnorm] = fitt(T, delta, xi, m, S, fitTc, fitexp, orthdist, in
 
             subplot(1, 3, 2)
             hold off
-            plot(tt, xit, '.b', 'MarkerSize', 4);
+            plot(tt2, xit, '.b', 'MarkerSize', 4);
             hold on
-            plot(ttt * trange, real((xirange * xif(ttt))), '-g');
+            plot(ttt2 * trange2, real((xirange * xif(ttt2))), '-g');
             hold off
             %title('correlation length scaling function');
             xlabel('(T-T_c)\delta^{-1/\nu}');
@@ -134,13 +193,17 @@ function [param, resnorm] = fitt(T, delta, xi, m, S, fitTc, fitexp, orthdist, in
 
             subplot(1, 3, 3)
             hold off
-            plot(tt, St, '.b', 'MarkerSize', 4);
-            hold on
-            plot(ttt * trange, real((Srange * Sf(ttt))), '-g');
-            hold off
+            semilogy(tt3, St, '.b', 'MarkerSize', 4);
+            if FitS == 1 
+
+                hold on
+                semilogy(ttt3 * trange3, real((Srange * Sf(ttt3))), '-g');
+                hold off
+            end
+            
             %title('correlation length scaling function');
             xlabel('(T-T_c)\delta^{-1/\nu}');
-            ylabel('S + c/6 log(\delta)');
+            ylabel( ' e^{ c S/6}  \delta^{1/\nu}');
             set(gca, 'fontsize', 16)
 
             drawnow
