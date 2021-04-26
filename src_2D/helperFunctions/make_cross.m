526f1342d002f767da9a01835114d122db81419a
David Devoogdt <david.devoogdt@gmail.com>
2021-04-26 14:17:48 +0200

    simplification 2D construction code

---
+++ src_2D/helperFunctions/make_cross.m
@@ -0,0 +1,16 @@
+function map =  make_cross( arr )   
+    mx=arr(1)+1;
+    my=arr(2)+1;
+
+    x= arr(1)+ arr(3)+1;
+    y= arr(2)+arr(4)+1;
+
+    map = zeros( [y,x]);
+    
+    map(my,mx) =1;
+    
+    map( my, 1: arr(1)   ) = 1;
+    map( my, mx: mx+arr(3)   ) = 1;
+    map( 1:arr(2), mx  ) = 1;
+    map( my:my+arr(4), mx  ) = 1;   
+end
